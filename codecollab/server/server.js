ea498a98bb890680b1a8eb227ec98b805c6bd4dc
KaivalZala <kaivalzala1636@gmail.com>
2025-06-05 11:02:09 +0530

    Update server.js

--- codecollab/server/server.js
+++ codecollab/server/server.js
@@ -63,14 +63,15 @@ const port = process.env.PORT || 4000;
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://code-collab-mu.vercel.app"],
     methods: ["GET", "POST"],
+    credentials: true
   },
 });
 
 connectDB();
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173","https://code-collab-mu.vercel.app"], credentials: true }));
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use(cookieParser());
